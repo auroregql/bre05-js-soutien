c83c9ae1bd6558cad12b582151f0c2276a6f30c9
auroregql <aurore.gicquel.06@gmail.com>
2026-01-09 12:58:10 +0000

    Exercice 1 terminé : Lire les éléments de plusieurs tableaux

---
+++ index.js
@@ -0,0 +1,11 @@
+function displayArray(array)
+{
+    for(let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+}
+
+displayArray([1, 2, 3, 4, 5]);
+displayArray([10, 20, 30, 40, 50]);
+displayArray([5, 4, 3, 2, 1]);
+displayArray([100, 200, 300, 400, 500]);
